ff48d1b773376f6b9271ee31943338da67fae160
wonjongin <59444360+wonjongin@users.noreply.github.com>
2020-03-23 01:11:58 +0900

    2020-03-23-MUN-01:11

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+function Food(props){
+  console.log(props);
+  return(
+    <h3>I like potato</h3>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>hello!!!</h1>
+      <Food fav="kimchi" />
     </div>
   );
 }
